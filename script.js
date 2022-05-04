72898f03bb24e684fab405d8729a8b4937d8aace
0xdiegoo <94507704+0xdiegoo@users.noreply.github.com>
2022-05-04 15:58:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = iniciar
+
+function iniciar() {
+    let btn = document.getElementById("btn");
+    btn.addEventListener("click", encriptar);
+    let btn2 = document.getElementById("desencriptar")
+    btn2.addEventListener("click", desencriptar)
+  }
+
+function encriptar(){
+  alert("que pelotudo que soy}")
+}
+
+function desencriptar(){
+  alert("sos recontra pelotudo")
+}
+
+  
